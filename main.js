820ce57bc743e6b8552a5ccc976dc24d6fbf07ce
Johnson0307 <82548146+Johnson0307@users.noreply.github.com>
2024-01-30 10:13:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('**Atividade 1**')
+var nome = 'Talefy'
+var sobreNome = ' Johnson'
+var nomeCompleto = nome.concat(sobreNome)
+console.log(nomeCompleto)
+console.log(nome.concat(sobreNome))
+
+console.log('**Atividade 2**')
+
+var n1 = 56
+var n2 = 38
+console.log(n1 * n2)
+var soma = n1 + n2
+console.log(soma)
